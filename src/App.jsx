bf2119789fd32d8d11aad41c391e791a513667de
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-12 17:38:12 +0500

    make the register form for the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Register from "./pages/Register";
-
+import './App.scss';
+import Register from './pages/Register';
+//import Login from './pages/Login';
 function App() {
   return (
-    <>
-      <Register />
-    </>
+   <>
+<Register/>
+   </>
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
+       <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form >
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="email " placeholder='email' />
+          <input style={{display:'none'}} type="file" id='file' />
+          <label htmlFor='file'>
+            sdsd
+          </label>
+          <button>Sign in</button>
+       
+        </form>
+     <p>You do have account? Login</p>
+      </div>
+    </div>
+    
     </div>
   )
 }
