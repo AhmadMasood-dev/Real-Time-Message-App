03183fc84a0074c2659fd9f71bd385950b851bae
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-13 21:11:08 +0500

    make the chats navbar which include icons of call and name of person

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -12,6 +12,26 @@ const Chats = () => {
           <span>Asjad</span>
           <p>Hello</p>
         </div>
+      </div>{" "}
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Asjad</span>
+          <p>Hello</p>
+        </div>
+      </div>{" "}
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Asjad</span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
   );
