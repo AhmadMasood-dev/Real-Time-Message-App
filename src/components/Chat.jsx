a961ca200b77715e623ef6901c8e802ca401daf7
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-16 02:10:37 +0500

    set chats of the user

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import Cam from '../img/cam.png'
 import Add from '../img/add.png'
 import More from '../img/more.png'
 import Messages from "./Messages.jsx";
 import Input from './Input.jsx'
+import { ChatContext } from "../context/chatContext.jsx";
 const Chat = () => {
+  const { data } = useContext(ChatContext);
+
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Zain</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
           <img src={More} alt="" />
         </div>
       </div>
-      <Messages/>
+      <Messages />
       <Input/>
     </div>
   );
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Message from "./Message.jsx";
+import { ChatContext } from "../context/chatContext.jsx";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firbase.js";
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
+
+  console.log(messages)
   return (
     <div className="messages">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
