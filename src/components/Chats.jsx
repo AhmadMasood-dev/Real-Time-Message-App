5d22297286e56a245c0145391a751d606ad52ccf
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-22 23:21:14 +0500

    set now side chats to see people that i search from dtabase and it shows when other user is saved

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,13 @@
-import { doc, onSnapshot } from "firebase/firestore";
 import React, { useState, useEffect, useContext } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../firbase";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/chatContext";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
-
   const { currentUser } = useContext(AuthContext);
   const { dispatch } = useContext(ChatContext);
-
   useEffect(() => {
     const getChats = () => {
       const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
@@ -23,30 +21,32 @@ const Chats = () => {
 
     currentUser.uid && getChats();
   }, [currentUser.uid]);
-
   const handleSelect = (u) => {
     dispatch({ type: "CHANGE_USER", payload: u });
   };
-
+  // ?.sort((a, b) => b[1].date - a[1].date)
   return (
     <div className="chats">
-      {Object.entries(chats)
-        ?.sort((a, b) => b[1].date - a[1].date)
-        .map((chat) => (
-          <div
-            className="userChat"
-            key={chat[0]}
-            onClick={() => handleSelect(chat[1].userInfo)}
-          >
-            <img src={chat[1].userInfo?.photoURL} alt="" />
-            <div className="userChatInfo">
-              <span>{chat[1].userInfo?.displayName}</span>
-              <p>{chat[1].lastMessage?.text}</p>
-            </div>
+      {Object.entries(chats)?.map((chat) => (
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Chats;
+
+/* 
+
+*/
